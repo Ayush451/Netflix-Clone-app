f71e16063e37918317fd2a866ecfbcc950225007
Ayush451 <88429944+Ayush451@users.noreply.github.com>
2022-08-12 02:25:16 +0100

    Main Component

---
+++ netflix-clone-app/src/Request.js
@@ -0,0 +1,11 @@
+const key= 'ca64a8111035b0f5db7f2428b3338c2f';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests;
